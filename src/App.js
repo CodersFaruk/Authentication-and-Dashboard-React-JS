5297745471de72c7e754621f50e08a62636954bd
CoderFaruk <66289933+CodersFaruk@users.noreply.github.com>
2022-04-09 11:43:58 +0600

    install react-bootstrap and react router Dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import { Button } from 'react-bootstrap'
 
 function App() {
   return (
     <>
      <h2>hello World</h2>
+     <Button variant="primary">Primary</Button>{' '}
     </>
   );
 }

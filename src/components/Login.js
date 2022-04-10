59d8dc7bf8d127a79447242031af39b7a80ffc3f
CoderFaruk <66289933+CodersFaruk@users.noreply.github.com>
2022-04-10 11:19:30 +0600

    Login Page Ui Compete

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import {Card,Form,Button} from 'react-bootstrap'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+    <Card>
+  <Card.Body>
+    <Card.Title className='font-weight-bold text-danger'>Login Your Information</Card.Title>
+    <Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group >
+  <Button variant="info"  type="submit">
+    LogIn
+  </Button>
+</Form>
+    
+  </Card.Body>
+</Card>
+    </>
   )
 }
 
